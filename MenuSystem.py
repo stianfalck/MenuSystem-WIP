24e615b571136ff1812df74383f0994447a3160f
Stian Falck <163509443+stianfalck@users.noreply.github.com>
2025-03-02 17:14:22 +0100

    Add files via upload

--- MenuSystem.py
+++ MenuSystem.py
@@ -21,6 +21,7 @@ class Menu():
         self.is_child = bool(is_child)
         self.parent = parent
         self.in_focus = False
+        self.root_menu = self.get_root()
         
         # Formatting constants
         self.ITEM_SPACING = 15
@@ -56,12 +57,20 @@ class Menu():
         # Initialize top parent menu in focus
         if self.is_parent and not self.is_child:
             self.set_focus(True)
+            self.active_menu = self
 
-        # Add menuitem to navigate to supermenu
+        # Add menuitem to navigate back to previous menu
         elif self.is_child:
             if self.parent:
                 back = MenuItem(self.parent.title, self.close, "option")
                 self.add(back)
+
+
+    def get_root(self):
+        if self.parent:
+            return self.parent.get_root()
+        return self
+
             
 
     def activate_buttons(self):
@@ -78,10 +87,12 @@ class Menu():
 
     
     def set_focus(self, value):
-        global current_menu
+        #global current_menu
         self.in_focus = bool(value)
         if value:
-            current_menu = self
+            #current_menu = self
+            root_menu = self.get_root()
+            root_menu.active_menu = self
             self.activate_buttons()
         else:
             self.deactivate_buttons()
@@ -103,15 +114,19 @@ class Menu():
         self.lcd.rect(self.title_x,  self.title_y, self.lcd.width, 8, self.BG_COLOR, True)
         self.lcd.rect(self.title_x-4, self.title_y-4, self.lcd.width-12, 16, self.GREEN)
         self.lcd.text(self.title, self.title_x, self.title_y, self.GREEN)
+        if self.is_child:
+            self.lcd.text("^", self.lcd.width-20, self.title_y, self.GREEN)
         self.lcd.show()
     
 
     def close(self):
-        global current_menu
+        #global current_menu
         if self.parent:
             self.set_focus(False)
             self.parent.set_focus(True)
-            current_menu = self.parent
+            root_menu = self.get_root()
+            root_menu.active_menu = self.parent
+            #current_menu = self.parent
 
     
     def add(self, item):
@@ -194,10 +209,10 @@ class Menu():
     
 
     def update(self):
-        if self.in_focus:
-            self.create_title()
-            self.navigate()
-            self.render()
+        if self.root_menu.active_menu.in_focus:
+            self.root_menu.active_menu.create_title()
+            self.root_menu.active_menu.navigate()
+            self.root_menu.active_menu.render()
 
 
 
@@ -225,8 +240,9 @@ class MenuItem:
     def run(self):
         global current_menu
         if isinstance(self.target, Menu):
-            self.target.set_focus(True)
-            current_menu = self.target
+            #current_menu = self.target
+            root_menu = self.target.get_root()
+            root_menu.active_menu = self.target
         else:
             self.target()
 
@@ -252,10 +268,14 @@ item_2 = MenuItem("Option 2", f2, "option")
 item_3 = MenuItem(label="Option 3", target=f3, type="option")
 
 sub_option = MenuItem("Sub Option 1", f1, "option")
-sub_menu = Menu(is_parent=False, is_child=True, title="SUB MENU", parent=menu)
+sub_menu = Menu(is_parent=True, is_child=True, title="SUB MENU", parent=menu)
 sub_menu.add(sub_option)
 sub_menu_item = MenuItem(label=sub_menu.title, target=sub_menu, type="submenu")
 
+
+sub_sub = Menu(True, True, "SUB SUB MENU", sub_menu)
+sub_sub_item = MenuItem(sub_sub.title, sub_sub, "submenu")
+sub_menu.add(sub_sub_item)
 
 menu.add(sub_menu_item)
 menu.add(item_2)
@@ -265,8 +285,8 @@ menu.add(item_3)
 menu.add(item_3)
 menu.add(item_3)
 
-
-current_menu = menu
+# keeps track of which menu the user is currently navigating
+#current_menu = menu
 
 while True:
-    current_menu.update()
+    menu.update()
